f5f34b693dba56c4fafe69cfa18bb7d0bee59c66
alfanidimas2-source <alfanidimas2@gmail.com>
2026-03-18 10:42:03 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'banser-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo_ansor.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetching assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
